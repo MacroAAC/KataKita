00b6056c6641b9cb556e3bec0f4b968d84c97d08
9wynneth <gwynnethgwen10@gmail.com>
2024-10-09 16:30:50 +0700

    added components that's customizable for text headline, custom button (for AAC cards, any buttons), and activity card. Also added color converter for hex

---
+++ KataKita/Component/ActivityCard.swift
@@ -0,0 +1,107 @@
+//
+//  ActivityCard.swift
+//  KataKita
+//
+//  Created by Gwynneth Isviandhy on 09/10/24.
+//
+
+import SwiftUI
+
+struct ActivityCard: View {
+    let icon: String?
+    let nomor: String
+    let text: String?
+    let font: Int
+    let width: Int
+    let height: Int
+    let iconWidth: Int?
+    let iconHeight: Int?
+    let bgColor: String
+    let bgTransparency: Double
+    let fontColor: String
+    let fontTransparency: Double
+    let cornerRadius: CGFloat
+    let isSystemImage: Bool
+    let action: (() -> Void)?
+
+    init(icon: String? = nil,nomor: String, text: String? = nil, width: Int, height: Int, font: Int, iconWidth: Int? = nil, iconHeight: Int? = nil, bgColor: String, bgTransparency: Double, fontColor: String, fontTransparency: Double, cornerRadius: CGFloat, isSystemImage: Bool = true, action: (() -> Void)? = nil) {
+        self.icon = icon
+        self.nomor = nomor
+        self.text = text
+        self.width = width
+        self.height = height
+        self.font = font
+        self.iconWidth = iconWidth
+        self.iconHeight = iconHeight
+        self.bgColor = bgColor
+        self.bgTransparency = bgTransparency
+        self.fontColor = fontColor
+        self.fontTransparency = fontTransparency
+        self.cornerRadius = cornerRadius
+        self.isSystemImage = isSystemImage
+        self.action = action
+    }
+
+    var body: some View {
+        Button(action: {
+            action?()
+        }) {
+            VStack {
+                HStack {
+                    Text(nomor)
+                        .font(.system(size: CGFloat(font), weight: .medium))
+                        .foregroundColor(Color(hex: fontColor, transparency: fontTransparency))
+                        .padding(.leading,8)
+                        .padding(.top,8)
+                        
+                    Spacer()
+                }
+                Spacer()
+                HStack(spacing: 4) {
+                    Spacer()
+                    
+                    if let icon = icon, !icon.isEmpty {
+                        if isSystemImage {
+                            Image(systemName: icon)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: CGFloat(iconWidth ?? 24), height: CGFloat(iconHeight ?? 24))
+                                .foregroundColor(Color(hex: fontColor, transparency: fontTransparency))
+                                .padding(.trailing,8)
+                        } else {
+                            Image(icon)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: CGFloat(iconWidth ?? 24), height: CGFloat(iconHeight ?? 24))
+                                .foregroundColor(Color(hex: fontColor, transparency: fontTransparency))
+                                .padding(.trailing,8)
+                        }
+                    }
+                    
+                    if let text = text, !text.isEmpty {
+                        Text(text)
+                            .font(.system(size: CGFloat(font), weight: .medium))
+                            .foregroundColor(Color(hex: fontColor, transparency: fontTransparency))
+                            .multilineTextAlignment(.center)
+
+                            
+                    }
+                    
+                    Spacer()
+                }.padding(.top,-15)
+                Spacer()
+            }
+        }
+        .buttonStyle(PlainButtonStyle())
+        .frame(width: CGFloat(width), height: CGFloat(height))
+        .background(Color(hex: bgColor, transparency: bgTransparency))
+        .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+        .shadow(radius:10)
+
+    }
+}
+
+// if it's image set isSystemImage to false , but if it's sfsymbols set it to true
+#Preview {
+    ActivityCard(icon: "ball", nomor: "1", text: "Ambil sikat gigimu dan taru di dalam", width: 260, height: 150, font: 15, iconWidth: 50, iconHeight: 50, bgColor: "#000000", bgTransparency: 1.0, fontColor: "#ffffff", fontTransparency: 1.0, cornerRadius: 20, isSystemImage: false)
+}
