260e037254da47f6fd168c4d7ea787fee5c3233a
AqillaSM <ashahbani@student.ciputra.ac.id>
2024-10-15 20:13:57 +0700

    revisi zoya

--- KataKita/ViewModel/AACBoardViewModel.swift
+++ KataKita/ViewModel/AACBoardViewModel.swift
@@ -11,14 +11,22 @@ class CardViewModel: ObservableObject {
 
     private func loadSampleData() {
         // Sample Categories
-        let category1 = Category(id: UUID(), name: "Work", color: "Red")
-        let category2 = Category(id: UUID(), name: "Personal", color: "Blue")
+        let Nouns = Category(id: UUID(), name: "Nouns", color: "Orange")
+        let Pronouns = Category(id: UUID(), name: "Pronouns", color: "Yellow")
+        let Verbs = Category(id: UUID(), name: "Verbs", color: "Green")
+        let Adjectives = Category(id: UUID(), name: "Adjectives", color: "Blue")
+        let PrepositionsSocialWords = Category(id: UUID(), name: "PrepositionsSocialWords", color: "Pink")
+        let Questions = Category(id: UUID(), name: "Questions", color: "Purple")
+        let NegationImportantWords = Category(id: UUID(), name: "NegationImportantWords", color: "Red")
+        let Adverbs = Category(id: UUID(), name: "Adverbs", color: "Brown")
+        let Conjunctions = Category(id: UUID(), name: "Conjunctions", color: "White")
+        let Determiners = Category(id: UUID(), name: "Determiners", color: "Grey")
 
-        categories = [category1, category2]
+        categories = [Nouns, Pronouns, Verbs, Adjectives, PrepositionsSocialWords, Questions, NegationImportantWords, Adverbs, Conjunctions, Determiners]
 
         // Sample Cards
-        let card1 = Card(id: UUID(), name: "sleep", image: UIImage(systemName: "sleep") ?? UIImage(), category: category1)
-        let card2 = Card(id: UUID(), name: "wake", image: UIImage(systemName: "wake") ?? UIImage(), category: category2)
+        let card1 = Card(id: UUID(), name: "sleep", image: UIImage(systemName: "sleep") ?? UIImage(), category: Verbs)
+        let card2 = Card(id: UUID(), name: "wake", image: UIImage(systemName: "wake") ?? UIImage(), category: Verbs)
 
         cards = [card1, card2]
     }
